ffa2fbd9906e5a94bbe31ad58a14b5db1ac22138
fuka-innovations <fuad.kalhori@gmail.com>
2021-12-28 22:09:09 +0200

    9.9: Patientor backend, step2

--- part2/index.ts
+++ part2/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-const cors = require('cors')
+const cors = require('cors')    // front end calls need
 const app = express();
 app.use(express.json());
 app.use(cors)
